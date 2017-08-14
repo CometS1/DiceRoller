077f56ff333b7f6b74b05d1e7a9aaa0d8faa3848
CometS1 <sppang1@yahoo.com>
2017-08-14 12:12:45 -0400

    Added log for dice rolls

--- DiceRoller.js
+++ DiceRoller.js
@@ -6,26 +6,39 @@ $(document).ready(function(){
 		var amount = parseInt($data.find('.amount').val());//Amount of dice to be rolled
 		var modifier = parseInt($data.find('.modifier').val());//Amount to add to the roll
 		var dieFaces = parseInt($data.find('.die').text());
+		var log = $("textarea");
 		var dieTotal = 0;
 		var roll = 0;
 		var sum = 0;
+
+		log.append("(");
 		for (i = 0; i < amount; i++){
 			roll = Math.floor((Math.random() * (dieFaces)) + 1 );
+			log.append(roll);
 			dieTotal = dieTotal + roll;
+
+			if (i !== amount-1){
+				log.append(" + ");
+			}
 		}
+		log.append(")");
 		
 		//Checking whether the modifier is positive or negative
 		var mod = $data.find('.plus').prop('checked');
 		if (mod){
+			log.append(" + " + modifier);
 			sum = dieTotal + modifier;
 		}
 		else{
+			log.append(" - " + modifier);
 			sum = dieTotal - modifier;
 		}
 		
+		log.append(" = " + sum + "\n");
 		$data.find('.sum').val(sum);
 		
-		
+		log.scrollHeight(99999);
+		log.scrollTop(99999);
 	});
 	
 });
